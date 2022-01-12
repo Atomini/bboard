543082619c1023c321ce9e4dab26408bbe08abee
Atomini <62155406+Atomini@users.noreply.github.com>
2022-01-12 22:01:39 +0200

    add registration and activation pages

--- bboard/main/apps.py
+++ bboard/main/apps.py
@@ -1,7 +1,19 @@
 from django.apps import AppConfig
+from django.dispatch import Signal
+
+from .utilities import send_activation_notification
+
+user_registered = Signal()
 
 
 class MainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'main'
     verbose_name = 'Доска обявлений'
+
+
+def user_registered_dispatcher(sender, **kwargs):
+    send_activation_notification(kwargs['instance'])
+
+
+user_registered.connect(user_registered_dispatcher)
--- bboard/main/urls.py
+++ bboard/main/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import index, other_page, BBLoginView, profile, BBLogoutView, ChangeUserInfoView, BBPasswordChangeView
+from .views import index, other_page, BBLoginView, profile, BBLogoutView, ChangeUserInfoView, BBPasswordChangeView, \
+    RegisterDoneView, RegisterUserView, user_activate
 
 app_name = "main"
 urlpatterns = [
+    path('accounts/register/activate/<str:sign>/', user_activate, name="register_activate"),
+    path('accounts/register/done', RegisterDoneView.as_view(), name="register_done"),
+    path('accounts/register', RegisterUserView.as_view(), name='register'),
     path('accounts/login/', BBLoginView.as_view(), name='login'),
     path('accounts/password/change', BBPasswordChangeView.as_view(), name='password_change'),
     path('<str:page>/', other_page, name='other'),
