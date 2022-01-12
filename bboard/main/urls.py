8a14f7bf20e9ade1fc55da174ec2667c73c23822
Atomini <62155406+Atomini@users.noreply.github.com>
2022-01-12 19:48:43 +0200

    add change passwrd page

--- bboard/main/urls.py
+++ bboard/main/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import index, other_page, BBLoginView, profile, BBLogoutView, ChangeUserInfoView
+from .views import index, other_page, BBLoginView, profile, BBLogoutView, ChangeUserInfoView, BBPasswordChangeView
 
 app_name = "main"
 urlpatterns = [
     path('accounts/login/', BBLoginView.as_view(), name='login'),
+    path('accounts/password/change', BBPasswordChangeView.as_view(), name='password_change'),
     path('<str:page>/', other_page, name='other'),
     path('', index, name='index'),
     path('accounts/profile/change', ChangeUserInfoView.as_view(), name='profile_change'),
--- bboard/main/views.py
+++ bboard/main/views.py
@@ -1,15 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 from django.template import TemplateDoesNotExist
 from django.template.loader import get_template
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, PasswordChangeView, LogoutView
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.views import LogoutView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import UpdateView
 from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
-from django.shortcuts import get_object_or_404
 
 from .models import AdvUser
 from .forms import ChangeUserInfoForm
@@ -57,3 +55,10 @@ class ChangeUserInfoView(SuccessMessageMixin, LoginRequiredMixin, UpdateView):
         if not queryset:
             queryset = self.get_queryset()
         return  get_object_or_404(queryset, pk=self.user_id)
+
+
+class BBPasswordChangeView(SuccessMessageMixin, LoginRequiredMixin, PasswordChangeView):
+    template_name = 'main/password_change.html'
+    success_url = reverse_lazy('main:profile')
+    success_message = 'Пароль пользователя изменен'
+
